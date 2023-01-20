6f4be2b6d26f1849840e6ed0db6e602e6acd7b9d
JamesDrosdick <jamesdrosdick@vt.edu>
2023-01-20 11:58:04 -0500

    James_new

--- James_new/James_MotorCombo.m
+++ James_new/James_MotorCombo.m
@@ -5,13 +5,13 @@ function D = James_MotorCombo(Combo_number)
     % outputs the struct "D" which contains all Data related to the information
     
     % cd into relevent folder
-    oldfolder = cd('CSVs');
+    %oldfolder = cd('CSVs');
     
     % Offset to ignore all the column headers    
     i = Combo_number+1; 
     
     % Read data in table
-    data = readmatrix('Acceptable_Motors3.csv','Range','A2:BS12');
+    data = readmatrix('Acceptable_Motors3.csv','Range','A2:CH12');
     
     % Thrust struct for all thrust values of a motor
     Thrust = [ data(1,12:26) ; data(i,12:26)]';
@@ -25,15 +25,17 @@ function D = James_MotorCombo(Combo_number)
     % Rpm  struct for all rpm values of a motor
     Rpm = [ data(1,57:71) ; data(i,57:71)]';
 
+    Voltage = [ data(1,72:86) ; data(i,72:86) ];
+
     % Compiles the static characteristics and then puts the structs above
     % into a larger struct so we can treat them all as one combination
-    data = readtable('Acceptable_Motors2.csv', "Delimiter", ',','Range','A3:BS12');
+    data = readtable('Acceptable_Motors3.csv', "Delimiter", ',','Range','A3:CH12');
     i = Combo_number;
     D = struct("Motor_Name", convertCharsToStrings(data.Var1(i)), "Cost", data.Var2(i), "Weight_g", data.Var3(i), "Kv", data.Var4(i),  ...
     "MotorDiam",data.Var5(i),"MotorLen", data.Var6(i), "ShaftDiam", data.Var7(i), "Volt_min", data.Var8(i), "Volt_max", data.Var9(i), "PropD", data.Var10(i),...
     "Prop_Pitch", data.Var11(i), ...
-    "Thrust_N", Thrust, "Current_A",Current, "Power_W", Power, "Rpm", Rpm); 
+    "Thrust_N", Thrust, "Current_A",Current, "Power_W", Power, "Rpm", Rpm, "Voltage", Voltage); 
     
     % cd out to main
-    cd(oldfolder);
+    % cd(oldfolder);
 end 
